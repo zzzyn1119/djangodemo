75097a12d52484a014cb70815677c0a0c0d4a0d9
zhangyanan <zhangyanan@shoppingm.cn>
2017-06-23 16:17:47 +0800

    On branch master  Changes to be committed: 	modified:   .gitignore 	modified:   functional_test.py

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,24 @@
 #! coding=utf-8
 
+import unittest
+import time
 from selenium import webdriver
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+	self.browser.implicitly_wait(3)    
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_open_url(self):
+        self.browser.get('http://localhost:8000')
+	#time.sleep(3)
+	print self.browser.title 
+	self.assertIn('Django', self.browser.title)
+	#self.fail('Finished the test!')
+        #assert 'Django' in browser.title
+
+if __name__=='__main__':
+    unittest.main()
