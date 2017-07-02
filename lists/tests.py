749113bbb3af3c2d5833f5cd764fe7df18204eff
zhangyanan <zhangyanan@shoppingm.cn>
2017-07-02 11:47:44 +0800

    atijiao

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.views import home_page
+from views import home_page
 
 # Create your tests here.
 '''
@@ -27,8 +27,14 @@ class HomePageTest(TestCase):
 
 	# self.assertIn(b'<title>To-Do lists</title>',response.content)
 	# self.assertTrue(response.content.strip().endswith(b'</html>'))
-
-	expected_html = render_to_string('home.html')
-	print expected_html
-	self.assertEqual(response.content.decode(), expected_html)
-
+	
+    def test_home_page_can_save_a_POST_request(self):
+	request = HttpRequest()
+	request.method =  'POST'
+	request.POST['item_text'] = 'A new list item'
+	
+	response = home_page(request)
+	self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html',{'new_item_text': 'A new list item'})
+        self.assertEqual(response.content.decode(), expected_html)
+	self.assertIn('A new list item', response.content.decode())
